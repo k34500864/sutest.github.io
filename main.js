3d06bef17c69f96af1cd1949b944e7ed825cb45a
Timcopt_11536 <141838836+k34500864@users.noreply.github.com>
2024-05-31 00:42:26 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+// Add JS here
+let menu=document.querySelector(".fas");
+let navbar=document.querySelector(".navbar");
+
+menu.onclick =() =>{
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+}
+window.onscroll =() =>{
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+}
